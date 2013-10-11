58f45e644a7fbd2ef9e13bf2e7e9d4b7d741774c
jmaddock <skimaddster@gmail.com>
2013-10-11 11:30:42 -0700

    updated for appengine

---
+++ app engine/test.py
@@ -0,0 +1,38 @@
+import webapp2
+from bs4 import BeautifulSoup
+import urllib
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        url = 'http://evergreenmtb.org/recreation/'
+        soup = BeautifulSoup(urllib.urlopen(url))
+        titles = soup.find_all("font")
+        dates = soup.find_all("td")
+        results = []
+        self.response.write(">> RIDES <<\n\n>> WORK PARTIES <<\n\n>> RACES <<\n\n>> CLASSES <<\n\n>> COMMUNITY MEETINGS <<\n\nhttp://evergreenmtb.org/recreation/calendar_view.php\n\n")
+        for x in titles[2:]:
+            if "#6D8D38" in str(x):
+                title = str(x.b)[3:-4]
+                if len(title) > 0 and "<center>" not in title:
+                    results.append({'title':title})
+        count = 0
+        for x in dates:
+            if 'td align="right"' in str(x):
+                date = str(x.b)
+                if 'PM' in date:
+                    results[count]['date'] = date[3:-4]
+                    count += 1
+                elif 'AM' in date:
+                    results[count]['date'] = date[3:-4]
+                    count += 1
+        for x in results:
+            self.response.write("> " + x['date'] + ": " + x['title'] + "\n")
+        self.response.write("\n\n")
+        for x in results:
+            self.response.write("> " + x['date'].partition('-')[0] + "- " + x['title'] + " @" + x['date'].partition('-')[2] + '\n')
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
---
+++ local/main.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import urllib
+
+url = 'http://evergreenmtb.org/recreation/'
+soup = BeautifulSoup(urllib.urlopen(url))
+titles = soup.find_all("font")
+dates = soup.find_all("td")
+results = []
+print ">> RIDES <<\n\n>> WORK PARTIES <<\n\n>> RACES <<\n\n>> CLASSES <<\n\n>> COMMUNITY MEETINGS <<\n\nhttp://evergreenmtb.org/recreation/calendar_view.php"
+for x in titles[2:]:
+    if "#6D8D38" in str(x):
+        title = str(x.b)[3:-4]
+        print title
+        if len(title) > 0:
+            results.append({'title':title})
+count = 0
+for x in dates:
+    if 'td align="right"' in str(x):
+        date = str(x.b)
+        if 'PM' in date:
+            results[count]['date'] = date[3:-4]
+            count += 1
+        elif 'AM' in date:
+            results[count]['date'] = date[3:-4]
+            count += 1
+for x in results:
+    print "> " + x['date'] + ": " + x['title']
+print "\n\n"
+for x in results:
+    print "> " + x['date'].partition('-')[0] + "- " + x['title'] + " @" + x['date'].partition('-')[2]
+#print(soup.prettify())
